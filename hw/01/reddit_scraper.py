99afbf76d265c14b0e7d6283991845a2ab0d8780
Stanislav Král <stanislav.kral3@gmail.com>
2021-02-27 18:20:14 +0100

    Added argparser

--- hw/01/reddit_scraper.py
+++ hw/01/reddit_scraper.py
@@ -35,6 +35,7 @@ class RedditScraper:
 
             # fetch the posts page target
             (page_data, cached) = self.fetch_target(target)
+            print("")
 
             # parse the xml tree
             tree = etree.HTML(page_data)
@@ -42,8 +43,8 @@ class RedditScraper:
             # find all things (posts)
             things = tree.xpath('//div[contains(@class, "thing")]')
 
-            # exclude video things
-            non_video_things = [thing for thing in things if not "data-kind" in thing.attrib or thing.attrib["data-kind"] != "video"]
+            # exclude video and promoted things
+            non_video_things = [thing for thing in things if thing.attrib.get("data-kind", "") != "video" and thing.attrib.get("data-promoted", "false") != "true"]
 
             # iterate over things and scrape title, text, timestamp, author, score, and comment
             for thing in non_video_things:
@@ -80,8 +81,10 @@ class RedditScraper:
                     post = {"title": title, "text": text, "timestamp": timestamp, "score": score, "comments_count": comments_count, "author": author} 
                     posts.append(post)
                     scraped_posts = scraped_posts + 1
-                    print(f"SCRAPED - {scraped_posts}/{self.target_post_count}")
-            
+                    print(f"SCRAPED - {scraped_posts}/{self.target_post_count}\n")
+                else:
+                    print(f"FOUND NON-TEXT THING\n")
+
                 if not thing_cached:
                     # be polite!!!
                     time.sleep(self.politeness_timeout)
@@ -93,7 +96,7 @@ class RedditScraper:
             next_page_link = tree.xpath('//div[contains(@class, "nav-buttons")]//span[contains(@class, "next-button")]/a/@href')
 
             # a link to the next page exists
-            if next_page_link is not empty:
+            if len(next_page_link) > 0:
                 next_target = next_page_link[0].replace(self.get_base_url(), "")
                 target_queue.append(next_target)
                 posts_loaded = len(text)
@@ -113,7 +116,7 @@ class RedditScraper:
         if page_data is None:  
             # if not then fetch it
             link = self.get_url(target) 
-            print(e"FETCHING - {link}")
+            print(f"FETCHING - {link}")
             response = requests.get(f"{link}", headers=self.headers)
             page_data = response.text
             # add the page to the cache
@@ -159,7 +162,6 @@ class ResourceCache:
         resource_path = f"{self.base_folder}/{resource_name}"
         if os.path.exists(resource_path):
             with open(resource_path, "r") as resource_file: 
-                print(f"{resource_name} cached")
                 return resource_file.read() 
         return None
 
@@ -169,14 +171,31 @@ if __name__ == "__main__":
     parser.add_argument(dest='subreddit', type=str, help="The subbredit to scrape")
 
     # Retur the input via different parameter name
-    parser.add_argument('-c', '--count', dest='posts_count', default=50)
+    parser.add_argument('-c', '--count', dest='posts_count', type=int, default=50)
     parser.add_argument('-cf', '--cache-folder', dest='cache_folder', default="cache")
+    parser.add_argument('-pt', '--politeness-timeout', dest='politiness_timeout', type=float, default=0.33)
+    parser.add_argument('-o', '--output', dest='output_file')
+    # TODO use refresh argument
+    parser.add_argument('-r', '--refresh', dest='refresh', default=False)
 
-    subreddit = parser.subreddit
-    cache = ResourceCache(parser.cache_folder, parser.subreddit)
-    scraper = RedditScraper(subreddit, 550, cache, 0.5)
+    # TODO use ignore stickied posts
+    parser.add_argument('-is', '--ignore-stickied', dest='ignore_stickied', default=True)
+
+    args = parser.parse_args()
+
+    subreddit = args.subreddit
+
+    if args.output_file is None:
+        args.output_file = f"{args.output_file}.json"
+
+    cache = ResourceCache(args.cache_folder, subreddit)
+    scraper = RedditScraper(subreddit, args.posts_count, cache, args.politiness_timeout)
     posts = scraper.scrape()
     json = json.dumps(posts)
-    with open(f"{subreddit}.json", "w") as output_file: 
+    output_file_name = args.output_file
+    with open(output_file_name, "w") as output_file: 
         output_file.write(json)
+        print(f"\nOUTPUT SAVED TO {output_file_name}")
+
+
 
